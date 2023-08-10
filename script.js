e0726a51f684cf938249010de04954adb3cf46b0
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-08-10 10:22:35 +0200

    Save and load if a checkbox is checked

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
-// -- GLOBAL --
-const LOCAL_STORAGE_PREFIX = "ADVANCED-TODO-LIST";
-const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
-
-const template = document.querySelector("#list-item-template");
 const form = document.querySelector("#new-todo-form");
 const todoInput = document.querySelector("#todo-input");
 const list = document.querySelector("#list");
-// Store user input into this empty array
-// const todos = [];
-const todos = loadTodos();
-todos.forEach((todo) => renderTodo(todo));
+const template = document.querySelector("#list-item-template");
+const LOCAL_STORAGE_PREFIX = "ADVANCED_TODO_LIST";
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
+let todos = loadTodos();
+todos.forEach(renderTodo);
 
-// Add todos
+list.addEventListener("change", (e) => {
+  if (!e.target.matches("[data-list-item-checkbox]")) return;
+
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+  const todo = todos.find((t) => t.id === todoId);
+  todo.complete = e.target.checked;
+  saveTodos();
+});
+
+list.addEventListener("click", (e) => {
+  if (!e.target.matches("[data-button-delete]")) return;
+
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+  parent.remove();
+  todos = todos.filter((todo) => todo.id !== todoId);
+  saveTodos();
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // Get user input
   const todoName = todoInput.value;
-
-  // Return if user input = ''
   if (todoName === "") return;
-
-  // Add user inputs to todos array
-  todos.push(todoName);
-
-  // Render todos to list
-  renderTodo(todoName);
-
-  // Save todos to local storage
-  saveTodos(todos);
-
-  // Clear and focus input after user submit
+  const newTodo = {
+    name: todoName,
+    complete: false,
+    id: new Date().valueOf().toString(),
+  };
+  todos.push(newTodo);
+  renderTodo(newTodo);
+  saveTodos();
   todoInput.value = "";
-  todoInput.focus();
 });
 
-function renderTodo(todoName) {
-  // Get HTML template
+function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true);
-
-  // Get particular span from template and set content to todo
-  const newTodo = templateClone.querySelector("[data-list-item-text]");
-  newTodo.textContent = todoName;
-
-  // Append todos on list
-  list.append(templateClone);
+  const listItem = templateClone.querySelector(".list-item");
+  listItem.dataset.todoId = todo.id;
+  const textElement = templateClone.querySelector("[data-list-item-text]");
+  textElement.innerText = todo.name;
+  const checkbox = templateClone.querySelector("[data-list-item-checkbox]");
+  checkbox.checked = todo.complete;
+  list.appendChild(templateClone);
 }
 
-// Delete todos
-// Complete todos
-
-// Load todos
 function loadTodos() {
   const todosString = localStorage.getItem(TODOS_STORAGE_KEY);
-  // Take a string and convert it to a JavaScript object or array (here: array)
-  // On the first load of the page => todosString is undefined, in this case
-  // we return an empty string
   return JSON.parse(todosString) || [];
 }
 
-// Save todos
-function saveTodos(todos) {
-  // Convert todos from a JSON array into a string with JSON.stringify()
-  // Save todos into local storage
-  localStorage.setItem(`${TODOS_STORAGE_KEY}`, JSON.stringify(todos));
+function saveTodos() {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
 }
